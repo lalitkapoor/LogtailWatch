02d0064be4f00cf4eb7e02b3edf5d2af5b2297c5
Lalit Kapoor <lalitkapoor@gmail.com>
2023-01-27 00:50:04 -0500

    updating readme to support new instructions

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+aws iam create-role --role-name logtail-watch-role --assume-role-policy-document file://role.json
+aws iam create-policy --policy-name logtail-watch-policy --policy-document file://policy.json
+aws iam attach-role-policy --policy-arn "<TBD-TO-FILL-HERE>" --role-name logtail-watch-role
+aws lambda create-function --function-name logtail-watch \
+  --runtime nodejs18.x \
+  --zip-file fileb://lambda.zip \
+  --handler src/router.handler \
+  --role "<TBD-TO-FILL-HERE>"
