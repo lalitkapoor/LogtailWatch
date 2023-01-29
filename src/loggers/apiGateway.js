72f7a5ec31a86c17da7d6aa5d7c12825451473f6
Lalit Kapoor <lalitkapoor@gmail.com>
2023-01-29 16:55:44 -0500

    fix: passing wrong object to getMeta

--- src/loggers/apiGateway.js
+++ src/loggers/apiGateway.js
@@ -8,14 +8,14 @@ let restApis = null;
 
 export default function (data, callback) {
   if (restApis == null) {
-    apiGateway.getRestApis({ limit: 500 }, function (err, data) {
+    apiGateway.getRestApis({ limit: 500 }, function (err, apis) {
       if (err) {
         console.warn("Unable to retrieve Rest APIs");
         console.warn(err.code, "-", err.message);
       } else {
         // Construct RestAPI map using id as key, name as value
-        if (data.items) {
-          restApis = data.items.reduce(function (map, api) {
+        if (apis.items) {
+          restApis = apis.items.reduce(function (map, api) {
             map[api.id] = api.name.replace(" ", "_");
             return map;
           }, {});
@@ -31,6 +31,7 @@ export default function (data, callback) {
 }
 
 const getMeta = function (data) {
+  console.log("api-gateway", data);
   // Construct the program name from the log group name
   let program = data.logGroup.split("_").splice(1).join("_").split("/");
   // If possible, replace the api ID with it's name
