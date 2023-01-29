310e702592e3d8fa1ba72edd3bc1ead8efdf557e
Lalit Kapoor <lalitkapoor@gmail.com>
2023-01-29 16:56:53 -0500

    add pino logger levels

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 export default {
   logLevelExtractor:
     ".* - (error|warn|info|verbose|debug|emerg|alert|crit|notice|silly): ",
+  pinoLevels: {
+    names: ["trace", "debug", "info", "warn", "error", "fatal"],
+    values: [10, 20, 30, 40, 50, 60],
+  },
   defaultLogLevel: "info",
 };
